dfa8d89c2d48e59de28bc3213d9aa67eab8e8b1a
claire Ghose <clairedevillers@hotmail.fr>
2021-05-13 19:38:35 -0400

    add server

--- src/components/AddPage/AddPage.jsx
+++ src/components/AddPage/AddPage.jsx
@@ -1,45 +1,78 @@
-import { Link } from 'react-router-dom'
+import React from "react";
 import './AddPage.css';
 
+class AddPage extends React.Component {
+  state= {
+    description:"",
+    amount:"",
+    date:"",
+    category:"",
+  }
 
-function AddPage(props) {
-  return (
-    <div className="AddPage">
-      <section className="AddPage-layout">
-        <h1>Add Expense</h1>
-        <div className="AddPage-form">
-          <h4>Description</h4>
-          <input />
-          <h4>Amount</h4>
-          <input type="number" min="0" step="any" />
-          <h4>Date</h4>
-          <input type="date" />
-          <h4>Choose category</h4>
+  changeInput = (event) => {
+    this.setState({[event.target.name]:event.target.value})
+  }
 
-        <div className="AddPage-btn-cat">
-          <i className="fa fa-cart-arrow-down" />
-          <i className="fa fa-home" />
-          <i className="fa fa-coffee" />
-          <i className="fa fa-cutlery" />
-          <i className="fa fa-paw" />
-          <i className="fa fa-heartbeat" />
-          <i className="fa fa-book" />
-          <i className="fa fa-phone" />
-          <i className="fa fa-plane" />
-          <i className="fa fa-music" />
-          <i className="fa fa-gamepad" />
-          <i className="	fa fa-wrench" />
-        </div>
+  send = async () => {
+    let fetchResponse = await fetch("https://jsonplaceholder.typicode.com/posts",
+    {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        description: this.state.description,
+        amount: this.state.amount,
+        date: this.state.date,
+        category: this.state.category,
+      })
+    })
+    let response = await fetchResponse.json()
+    console.log(response)
+    this.props.history.push('/list')
+  }
+  
+  render() {
+    return (
+      <div className="AddPage">
+        <section className="AddPage-layout">
+          <h1>Add Expense</h1>
+          <div className="AddPage-form">
 
-        </div>
+            <h4>Description</h4>
+            <input onChange={this.changeInput} name="description" value={this.state.description} />
 
-        <div className="AddPage-btn">
-          <button className="btn btn-warning">Add expense</button>
-        </div>
+            <h4>Amount</h4>
+            <input type="number" onChange={this.changeInput} name="amount" value={this.state.amount} />
 
-      </section>
+            <h4>Date</h4>
+            <input type="date" onChange={this.changeInput} name="date" value={this.state.date} />
 
-    </div>
-  );
+            <h4>Choose category</h4>
+
+            <div className="AddPage-btn-cat">
+              <i className="fa fa-cart-arrow-down" />
+              <i className="fa fa-home" />
+              <i className="fa fa-coffee" />
+              <i className="fa fa-cutlery" />
+              <i className="fa fa-paw" />
+              <i className="fa fa-heartbeat" />
+              <i className="fa fa-book" />
+              <i className="fa fa-phone" />
+              <i className="fa fa-plane" />
+              <i className="fa fa-music" />
+              <i className="fa fa-gamepad" />
+              <i className="	fa fa-wrench" />
+            </div>
+
+          </div>
+
+          <div className="AddPage-btn">
+            <button className="btn btn-warning" onClick={() => {this.send()}}>Add expense</button>
+          </div>
+
+        </section>
+
+      </div>
+    );
+  }
 }
 export default AddPage
